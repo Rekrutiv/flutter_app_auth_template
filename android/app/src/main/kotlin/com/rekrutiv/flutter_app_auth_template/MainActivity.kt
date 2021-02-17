1815c56b16f600cc9a91e49a2df5dc5840e2faa9
VOLODYMYR <rekrutivvolodymyr@gmail.com>
2021-02-17 13:59:37 +0200

    firebase google auth

---
+++ android/app/src/main/kotlin/com/rekrutiv/flutter_app_auth_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rekrutiv.flutter_app_auth_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_app_auth_template/app/observers/app_bloc_observer.dart';
+import 'package:flutter_app_auth_template/authenticaiton/bloc/authentication_bloc.dart';
+import 'package:flutter_app_auth_template/authenticaiton/data/providers/google_sign_in_provider.dart';
+import 'package:flutter_app_auth_template/authenticaiton/data/repositories/authenticaiton_repository.dart';
+import 'package:flutter_app_auth_template/home/views/home_main_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'authenticaiton/data/providers/authentication_firebase_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = AppBlocObserver();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AuthenticationBloc(
+        authenticationRepository: AuthenticationRepository(
+          authenticationFirebaseProvider: AuthenticationFirebaseProvider(
+            firebaseAuth: FirebaseAuth.instance,
+          ),
+          googleSignInProvider: GoogleSignInProvider(
+            googleSignIn: GoogleSignIn(),
+          ),
+        ),
+      ),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeMainView(),
+      ),
+    );
+  }
+}
