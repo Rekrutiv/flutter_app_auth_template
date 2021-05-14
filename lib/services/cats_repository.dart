df2999cfc05d20d2be1f9081d3697056ca2f8698
VOLODYMYR <rekrutivvolodymyr@gmail.com>
2021-05-14 18:31:32 +0300

    10 item

--- lib/services/cats_repository.dart
+++ lib/services/cats_repository.dart
@@ -10,7 +10,7 @@ class CatsRepository {
 
   Future<List<Cat>> getData(int pageIndex) async {
     List<Cat> newCats = [];
-    final url = 'https://api.thecatapi.com/v1/breeds?page=$pageIndex&limit=15';
+    final url = 'https://api.thecatapi.com/v1/breeds?page=$pageIndex&limit=10';
     final apikey = '084ba1e9-75a2-47d1-9bc0-4a773f8e18dc';
     Response response = await get(url, headers: {'x-api-key': apikey});
     Iterable l = json.decode(response.body);
